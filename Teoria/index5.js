f1c19b2fcb6000cf446070cf92935780f35123b9
Fernando Warno <ferwar@hotmail.com>
2022-10-08 21:05:25 -0300

    funciones2

--- Teoria/index5.js
+++ Teoria/index5.js
@@ -2,17 +2,17 @@
 // var: viven en todo el codigo, dentro y fuera de bloques de codigo.
 // let: funciona segun el ambito local
 
-var resultado = "capuchino";
+let resultado = "capuchino";
 function cafetera(ingrediente1) {
-    
-    if(ingrediente1 == 'leche') {
-        return resultado
-    }
+  if (ingrediente1 == "leche") {
+    let resultado = "Otra cosa";
+    return resultado;
+  }
 }
 
 // var resultado = "capuchino";
 
-var taza = cafetera('leche')
+var taza = cafetera("leche");
 console.log(taza);
 
 //var resultado = "capuchino";
